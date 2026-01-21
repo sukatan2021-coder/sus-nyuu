85fba723512a87774a67a098a624db149c3c6101
sukatan2021-coder <sukatan2021@gmail.com>
2026-01-21 22:55:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,24 @@
-const CACHE_NAME = "nyuuka-pwa-fixed-v1";
-const ASSETS = ["./","./index.html","./manifest.json","./sw.js","./icon-192.png","./icon-512.png"];
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+const CACHE_NAME='emulsion-cache-v4';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',(e)=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener("activate", (e) => {
-  e.waitUntil(self.clients.claim());
+
+self.addEventListener('activate',(e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null)))
+  );
+  self.clients.claim();
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+
+self.addEventListener('fetch',(e)=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>res||fetch(e.request).then(net=>{
+      const copy=net.clone();
+      caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));
+      return net;
+    }).catch(()=>caches.match('./index.html')))
+  );
 });
