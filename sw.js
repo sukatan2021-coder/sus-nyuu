b6c4ba5ff7ad03fc9e79b154a5badc232fc029d2
sukatan2021-coder <sukatan2021@gmail.com>
2026-01-22 00:06:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-const CACHE_NAME='emulsion-cache-v4';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE_NAME = "nyuu-cache-v3m-alert-1";
+const ASSETS = ["./","./index.html","./manifest.json","./sw.js","./icon-192.png","./icon-512.png"];
 
-self.addEventListener('install',(e)=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate',(e)=>{
+self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null)))
+    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME ? caches.delete(k):null)))
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch',(e)=>{
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res=>res||fetch(e.request).then(net=>{
-      const copy=net.clone();
-      caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));
-      return net;
-    }).catch(()=>caches.match('./index.html')))
+    caches.match(e.request).then((r) => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+      return resp;
+    }).catch(()=>caches.match("./index.html")))
   );
 });
